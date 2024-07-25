313604780b24052ea869da55be38115a4dd8cc88
Abdulaziz PC <abdulaziz.khmail@gmail.com>
2024-07-25 16:11:46 +0500

    market page route fix

--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -25,30 +25,29 @@
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-6 justify-content-center">
                @foreach($products as $product)
                     <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">{{ $product->name }}</h5>
-                                <!-- Product price-->
-                                ${{ $product->price }}
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">{{ $product->name }}</h5>
+                                    <!-- Product price-->
+                                    ${{ $product->price }}
+                                </div>
+                                <div class="text-center">
+                                    <!-- Seller name -->
+                                    {{ $product->user_id }}
+                                </div>
                             </div>
-                            <div class="text-center">
-                                <!-- Seller name -->
-                                {{ $product->user_id }}
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('overview', $product) }}">Overview</a></div>
                             </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('overview', $product) }}">Overview</a></div>
                         </div>
                     </div>
-                </div>
-                @endforeach
-                <div>
+               @endforeach
 {{--                    <div class="col mb-5">--}}
 {{--                        <div class="card h-100">--}}
 {{--                            <!-- Sale badge-->--}}
@@ -226,7 +225,6 @@
 {{--                        </div>--}}
 {{--                    </div>--}}
 {{--                </div>--}}
-                </div>
             </div>
         </div>
     </section>
