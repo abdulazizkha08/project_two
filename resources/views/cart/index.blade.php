bdd26f23582be9969c7cc6a4ce1c1df667d250ae
Abdulaziz PC <abdulaziz.khmail@gmail.com>
2024-07-30 15:49:35 +0500

    shopping cart almost ready 2

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,3 +1,4 @@
+@php use App\Helpers\ProductHelper; @endphp
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -28,8 +29,8 @@
                             $price = $product['price'];
                             $quantity = $item['quantity'];
                             $totalPrice = $price * $quantity;
-                            $formattedPrice = \App\Helpers\ProductHelper::formatPrices(collect([$price]))->first();
-                            $formattedTotalPrice = \App\Helpers\ProductHelper::formatPrices(collect([$totalPrice]))->first();
+                            $formattedPrice = ProductHelper::formatPrices(collect([$price]))->first();
+                            $formattedTotalPrice = ProductHelper::formatPrices(collect([$totalPrice]))->first();
                         @endphp
                         <tr>
                             <td>{{ $product['name'] }}</td>
@@ -48,7 +49,7 @@
                     </tbody>
                 </table>
                 <div class="text-right">
-                    <strong>Total: {{ \App\Helpers\ProductHelper::formatPrices(collect([array_reduce(session('cart'), function($carry, $item) {
+                    <strong>Total: {{ ProductHelper::formatPrices(collect([array_reduce(session('cart'), function($carry, $item) {
                         return $carry + ($item['product']['price'] * $item['quantity']);
                     }, 0)]))->first() }} UZS</strong>
                 </div>
@@ -56,35 +57,35 @@
                 <p>Your cart is empty.</p>
             @endif
         </div>
-{{--        <div class="container px-4 px-lg-5 market-width">--}}
-{{--            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-6 justify-content-center">--}}
-{{--                @foreach($products as $product)--}}
-{{--                    <div class="col mb-5">--}}
-{{--                        <div class="card h-100">--}}
-{{--                            <!-- Product image-->--}}
-{{--                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />--}}
-{{--                            <!-- Product details-->--}}
-{{--                            <div class="card-body p-4">--}}
-{{--                                <div class="text-center">--}}
-{{--                                    <!-- Product name-->--}}
-{{--                                    <h5 class="fw-bolder">{{ $product->name }}</h5>--}}
-{{--                                    <!-- Product price-->--}}
-{{--                                    {{ $product->price }} UZS--}}
-{{--                                </div>--}}
-{{--                                <div class="text-center">--}}
-{{--                                    <!-- Seller name -->--}}
-{{--                                    <a href="">{{ $product->user->name }}</a>  --}}{{--href="{{ route('overview', $user) }}"--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                            <!-- Product actions-->--}}
-{{--                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">--}}
-{{--                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('cart.remove', $product) }}">Remove</a></div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        {{--        <div class="container px-4 px-lg-5 market-width">--}}
+        {{--            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-6 justify-content-center">--}}
+        {{--                @foreach($products as $product)--}}
+        {{--                    <div class="col mb-5">--}}
+        {{--                        <div class="card h-100">--}}
+        {{--                            <!-- Product image-->--}}
+        {{--                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />--}}
+        {{--                            <!-- Product details-->--}}
+        {{--                            <div class="card-body p-4">--}}
+        {{--                                <div class="text-center">--}}
+        {{--                                    <!-- Product name-->--}}
+        {{--                                    <h5 class="fw-bolder">{{ $product->name }}</h5>--}}
+        {{--                                    <!-- Product price-->--}}
+        {{--                                    {{ $product->price }} UZS--}}
+        {{--                                </div>--}}
+        {{--                                <div class="text-center">--}}
+        {{--                                    <!-- Seller name -->--}}
+        {{--                                    <a href="">{{ $product->user->name }}</a>  --}}{{--href="{{ route('overview', $user) }}"--}}
+        {{--                                </div>--}}
+        {{--                            </div>--}}
+        {{--                            <!-- Product actions-->--}}
+        {{--                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">--}}
+        {{--                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('cart.remove', $product) }}">Remove</a></div>--}}
+        {{--                            </div>--}}
+        {{--                        </div>--}}
+        {{--                    </div>--}}
+        {{--                @endforeach--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
     </section>
     <!-- Footer-->
     <footer class="bg-dark footer-c-style">
